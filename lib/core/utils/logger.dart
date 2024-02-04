24f053b7070d6c2de03db36c902d08c3442167fb
Baharudin Yusup <baharudin.yusup@outlook.com>
2024-02-04 23:42:06 +0700

    Minor changes: update logger message and rebuild freezed

--- lib/core/utils/logger.dart
+++ lib/core/utils/logger.dart
@@ -1,19 +1,20 @@
+import 'dart:core' as core;
 import 'dart:developer';
 
 class Logger {
   Logger._();
 
   static void print(
-    Object message, {
-    String name = '',
+    core.Object message, {
+    core.String name = '',
   }) {
     return log('$message', name: name);
   }
 
   static void error(
-    Object error, {
-    required String event,
-    String name = '',
+    core.Object? error, {
+    required core.String event,
+    core.String name = '',
   }) {
     return log('An error occurred when $event', name: name, error: error);
   }
