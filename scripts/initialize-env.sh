97390d06f25f5588be2cb10968c81051fddb397c
Baharudin Yusup <baharudin.yusup@outlook.com>
2024-06-04 16:59:17 +0700

    Added .sh script files

---
+++ scripts/initialize-env.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# Source and destination variables
+fileName="ENV"
+sourceFile="../configs/$ENV/.env"
+destinationFolder="../"
+
+echo "Initialize $ENV $fileName started..."
+
+# Check if environment variable is set
+if [ -z "$ENV" ]; then
+    echo "Environment variable ENV is not set."
+    exit 1
+fi
+
+# Check if source file exists
+if [ ! -f "$sourceFile" ]; then
+    echo "$fileName source file not found: $sourceFile"
+    exit 1
+fi
+
+# Check if destination folder exists
+if [ ! -d "$destinationFolder" ]; then
+    echo "Destination folder not found: $destinationFolder"
+    exit 1
+fi
+
+# Copy the file and overwrite if it exists
+if cp "$sourceFile" "$destinationFolder"; then
+    echo "$fileName file successfully copied and overwritten!"
+else
+    echo "Failed to copy the $fileName file."
+    exit 1
+fi
