c881135e00d9e7eeb5cbe7255d56a64ef9e2e2ad
Baharudin Yusup <baharudin.yusup@avisbudget.com>
2024-06-15 02:12:32 +0700

    Bugfix:
    - Timestamp data model is cannot convert fromJson
    - Minor UI bug in setup screen
    - Video call iOS permission problem

--- lib/data/models/user_model.dart
+++ lib/data/models/user_model.dart
@@ -67,8 +67,16 @@ class UserModel extends BasicUserModel {
         updatedAt: isNewAccount ? Timestamp.now() : null,
       );
 
-  factory UserModel.fromJson(Map<String, dynamic> json) =>
-      _$UserModelFromJson(json);
+  factory UserModel.fromJson(Map<String, dynamic> json) {
+    const converter = TimestampConverter();
+    if (json['createdAt'].runtimeType == Timestamp) {
+      json['createdAt'] = converter.toJson(json['createdAt']);
+    }
+    if (json['updatedAt'].runtimeType == Timestamp) {
+      json['updatedAt'] = converter.toJson(json['updatedAt']);
+    }
+    return _$UserModelFromJson(json);
+  }
 
   @override
   Map<String, dynamic> toJson() => _$UserModelToJson(this);
